bd1929b82c72fa4891b92a3a48c8cf59614bd6fc
Jonathan Lee <jonathancklee@gmail.com>
2026-01-31 19:52:57 +1100

    Implement copy to clipboard func

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,21 +1,34 @@
+import { useState } from 'react';
 import useGradientStore from '../store/GradientStore';
 
 export function CodeEditor() {
     const gradient = useGradientStore();
     const css = gradient.getCssString() ?? '';
-    const lines = css.split('\n');
-
+    const [isCopied, setIsCopied] = useState(false);
+    const copyText = async () => {
+        try {
+            await navigator.clipboard.writeText(css);
+            setIsCopied(true);
+            setTimeout(() => setIsCopied(false), 2000);
+        } catch (err) {
+            console.error('Failed to copy text: ', err);
+        }
+    };
     return (
-        <div className="mockup-code glassmorphism w-full bg-indigo-800/70">
+        <div className="mockup-code glassmorphism relative w-full bg-indigo-800/80">
             <div className="p-2">
-                {lines.map((line, i) => (
-                    <pre data-prefix=">" key={i} className="flex items-start">
-                        <code className="flex-1 font-mono wrap-break-word whitespace-pre-wrap">
-                            {line}
-                        </code>
-                    </pre>
-                ))}
+                <pre data-prefix=">" className="flex items-start">
+                    <code className="flex-1 font-mono wrap-break-word whitespace-pre-wrap">
+                        {css}
+                    </code>
+                </pre>
             </div>
+            <button
+                className="glassmorphism btn btn-outline absolute top-2 right-2 hover:text-indigo-600"
+                onClick={copyText}
+            >
+                {isCopied ? 'Copied!' : 'Copy to clipboard'}
+            </button>
         </div>
     );
 }
